1f5eb9ed6ccac21c5e84c462adb99f111d0078ae
amirAHS2000 <ahs.amirhosein78@gmail.com>
2021-12-29 22:38:03 +0330

    complete datastore repository class

--- app/src/main/java/com/example/pharmacyapp/data/DataStoreRepository.kt
+++ app/src/main/java/com/example/pharmacyapp/data/DataStoreRepository.kt
@@ -1,3 +1,61 @@
 package com.example.pharmacyapp.data
 
-class DataStoreRepository
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.*
+import androidx.datastore.preferences.preferencesDataStore
+import com.example.pharmacyapp.util.Constant.Companion.PREFERENCE_NAME
+import com.example.pharmacyapp.util.Constant.Companion.PREFERENCE_USER_ID
+import com.example.pharmacyapp.util.Constant.Companion.PREFERENCE_USER_TOKEN
+import javax.inject.Inject
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+private val Context.datastore by preferencesDataStore(PREFERENCE_NAME)
+
+@ViewModelScoped
+class DataStoreRepository @Inject constructor(@ApplicationContext private val context: Context) {
+
+    private object PreferenceKeys {
+        val userIdKey = intPreferencesKey(PREFERENCE_USER_ID)
+        val userTokenKey = stringPreferencesKey(PREFERENCE_USER_TOKEN)
+    }
+
+    private val dataStore: DataStore<Preferences> = context.datastore
+
+    suspend fun saveUserData(
+        userID: Int,
+        userToken: String
+    ) {
+        dataStore.edit { preferences ->
+            preferences[PreferenceKeys.userIdKey] = userID
+            preferences[PreferenceKeys.userTokenKey] = userToken
+        }
+    }
+
+    val readUserData: Flow<UserDataStore> = dataStore.data
+        .catch { exception ->
+            if (exception is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw exception
+            }
+        }
+        .map { preferences ->
+            val userId = preferences[PreferenceKeys.userIdKey] ?: 0
+            val userToken = preferences[PreferenceKeys.userTokenKey] ?: "none"
+            UserDataStore(
+                userId,
+                userToken
+            )
+        }
+}
+
+data class UserDataStore(
+    val userId: Int,
+    val userToken: String
+)
--- app/src/main/java/com/example/pharmacyapp/util/Constant.kt
+++ app/src/main/java/com/example/pharmacyapp/util/Constant.kt
@@ -2,6 +2,12 @@ package com.example.pharmacyapp.util
 
 class Constant {
     companion object {
+        //base url (pharmacy api)
         const val BASE_URL = "http://5.56.132.82:8080/"
+
+        //datastore preference
+        const val PREFERENCE_NAME = "user_data"
+        const val PREFERENCE_USER_ID = "user_id"
+        const val PREFERENCE_USER_TOKEN = "user_token"
     }
 }
